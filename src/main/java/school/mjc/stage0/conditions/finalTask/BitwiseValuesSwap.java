713cd5f1c623a4a71ff3c2d96a16e2c29354bba9
medjik <med.akhmetov@gmail.com>
2023-08-15 12:32:59 +0600

    third attempt

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -1,11 +1,7 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
-    public static void main(String[] args) {
-        swap(5,6);
-        swap(6,7);
-    }
-    public static void swap(int first, int second) {
+    public void swap(int first, int second) {
         second= first^second;
         first=(second^first);
         second=second^first;

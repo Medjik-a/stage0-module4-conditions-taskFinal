bffefca2131f9bce309afa6594b3e880c248451d
medjik <med.akhmetov@gmail.com>
2023-08-15 12:23:20 +0600

    first attempt

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        second= first^second;
+        first=(second^first)|first;
+        second=second^first;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -1,6 +1,21 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import java.util.SortedMap;
+
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if(x>0&&y>0){
+            System.out.println("first");
+        } else if (x>0&&y<0) {
+            System.out.println("fourth");
+
+        } else if (x<0&&y>0) {
+            System.out.println("second");
+        } else if (x<0&&y<0) {
+            System.out.println("third");
+        }
+        else{
+            System.out.println("zero");
+        }
     }
 }
